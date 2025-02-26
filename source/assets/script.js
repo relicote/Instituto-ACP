b3e78c4305b5b744d0d88e600909d5cc655fd81c
Gabriel Reliquias <relicote@gmail.com>
2025-02-26 11:49:45 -0300

    Adicionado links e editado campos solicitados

--- source/assets/script.js
+++ source/assets/script.js
@@ -16,3 +16,11 @@ const elements = [
 elements.forEach(selector => {
     ScrollReveal().reveal(selector, { delay: 100, origin: 'bottom', distance: '50px', duration: 1000 });
 });
+
+document.getElementById("inscreverProjeto").addEventListener("click", function(){
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSerulCSQ45Y1j3HIZi7KbUVcvP9Oqakv7drF_iekeKGSzFzEw/viewform";
+});
+
+document.getElementById("inscreverIniciativa").addEventListener("click", function(){
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSerulCSQ45Y1j3HIZi7KbUVcvP9Oqakv7drF_iekeKGSzFzEw/viewform";
+});
